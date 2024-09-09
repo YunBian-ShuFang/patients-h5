2bfd61b663b18bcf9432fa0925cd9631eea0cfd3
kohane <13542031914@163.com>
2024-09-09 20:08:52 +0800

    移动端适配

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  plugins: {
+    'postcss-px-to-viewport': {
+      // 设备狂赌375计算vw的值
+      viewportWidth: 375
+    }
+  }
+}
+// pnpm add -D postcss-px-to-viewport
