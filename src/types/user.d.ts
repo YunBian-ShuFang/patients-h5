c218438e9b2004cc68c64b14ba650b72dbe9c9c3
kohane <13542031914@163.com>
2024-09-09 20:10:46 +0800

    用户状态仓库

---
+++ src/types/user.d.ts
@@ -0,0 +1,13 @@
+// 用户信息
+export type User = {
+  /* token 令牌 */
+  token: string
+  /* 用户 ID */
+  id: string
+  /* 用户名称 */
+  account: string
+  /* 手机号 */
+  mobile: number
+  /* 头像 */
+  avatar: string
+}
