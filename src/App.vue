483f1546798a2ead30144de5175d0fa4fabc6578
kohane <13542031914@163.com>
2024-09-09 21:45:27 +0800

    请求工具函数-- axios 二次封装

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <script setup lang="ts">
 import { Button as VanButton } from 'vant'
 import { useUserStore } from './stores'
+import { request } from '@/utils/request'
+import type { User } from './types/user'
 
+// 测试，请求拦截器，是否携带token，响应拦截器401拦截到登陆地址
+const getUserInfo = async () => {
+  const res = await request('/patient/myUser')
+  console.log(res)
+}
+// 测试，响应拦截器,出现非10000的情况，和返回玻璃后的数据
 const store = useUserStore()
+const login = async () => {
+  const res = await request<User>('/login/password', 'post', {
+    mobile: '13211112222',
+    // 密码 abc123456 测试：出现非10000的情况
+    password: 'abc123456'
+  })
+  store.setUser(res.data)
+}
 </script>
 
 <template>
-  <p>{{ store.user }}</p>
-  <van-button
-    type="primary"
-    @click="
-      store.setUser({
-        id: '1',
-        mobile: 15595614402,
-        account: '1',
-        token: '1',
-        avatar: '1'
-      })
-    "
-    >登陆</van-button
-  >
-  <van-button @click="store.delUser()">退出</van-button>
+  <van-button type="primary" @click="getUserInfo">获取个人信息</van-button>
+  <van-button type="primary" @click="login">登陆</van-button>
 </template>
 
 <style scoped lang="scss">
---
+++ src/utils/request.ts
@@ -0,0 +1,69 @@
+import router from '@/router'
+import { useUserStore } from '@/stores'
+import axios, { AxiosError, type Method } from 'axios'
+import { showToast } from 'vant'
+
+export const baseURL = 'https://consult-api.itheima.net/'
+// 创建 axios 实例，基础配置
+const instance = axios.create({
+  baseURL,
+  timeout: 10000
+})
+
+// 请求拦截器，携带token
+instance.interceptors.request.use(
+  (config) => {
+    const store = useUserStore()
+    if (store.user?.token && config.headers) {
+      config.headers['Authorization'] = `Bearer ${store.user?.token}`
+    }
+    return config
+  },
+  (err) => Promise.reject(err)
+)
+
+// 响应拦截器，剥离无效数据，401拦截
+instance.interceptors.response.use(
+  (res) => {
+    // 后台约定，响应成功，但是 code 不是10000，是业务逻辑失败
+    if (res.data?.code !== 10000) {
+      showToast(res.data?.message || '业务失败')
+      return Promise.reject(res.data)
+    }
+    // 业务逻辑成功，返回响应数据，作为 axios 成功的结果
+    return res.data
+  },
+  (err) => {
+    if (err.response.status === 401) {
+      // 删除用户信息
+      const store = useUserStore()
+      store.delUser()
+      // 跳转登陆，带上接口失败所在页面的地址，登陆完成后回跳使用
+      router.push({
+        path: '/login',
+        query: { returnUrl: router.currentRoute.value.fullPath }
+      })
+    }
+    return Promise.reject(err)
+  }
+)
+
+type Data<T> = {
+  code: number
+  message: string
+  data: T
+}
+// 请求工具函数
+const request = <T>(
+  url: string,
+  method: Method = 'get',
+  submitData?: object
+) => {
+  return instance.request<T, Data<T>>({
+    url,
+    method,
+    [method.toUpperCase() === 'get' ? 'params' : 'data']: submitData
+  })
+}
+
+export { instance, request }
