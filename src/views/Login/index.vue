18fece7bda7b65cbd7466e0d7f2034c2b12d6d58
kohane <13542031914@163.com>
2024-09-11 19:53:20 +0800

    login登陆页初步开发

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const handleRightClick = () => {
+  alert('右侧按钮被点击了！')
+}
+</script>
+
+<template>
+  <div class="login-page">
+    <cp-nav-bar
+      title="登陆"
+      rightText="注册"
+      @click-right="handleRightClick"
+    ></cp-nav-bar>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
