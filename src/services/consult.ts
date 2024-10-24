0a80843e0366bc553c549d988048d3b9f61a7fae
kohane <13542031914@163.com>
2024-10-24 16:41:06 +0800

    首页-关注-推荐关注医生轮播数据渲染

--- src/services/consult.ts
+++ src/services/consult.ts
@@ -1,4 +1,4 @@
-import type { KnowledgeParams } from '@/types/consult'
+import type { FollowType, KnowledgeParams, PageParams } from '@/types/consult'
 import { instance } from '@/utils/request'
 
 // 首页-查询推荐/减脂/饮食健康/关注页面--百科文章列表
@@ -7,5 +7,23 @@ export const getKnowledgePage = (params: KnowledgeParams) => {
     url: '/patient/home/knowledge',
     method: 'get',
     params
+  })
+}
+
+// 首页-关注的医生列表
+export const getDoctorPage = (params: PageParams) => {
+  return instance({
+    url: '/home/page/doc',
+    method: 'get',
+    params
+  })
+}
+
+// 关注-关注操作
+export const followOrUnfollow = (id: string, type: FollowType = 'doc') => {
+  return instance({
+    url: '/like',
+    method: 'post',
+    data: { id, type }
   })
 }
---
+++ src/views/Home/components/DoctorCard.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { useFollow } from '@/composable'
+import type { Doctor } from '@/types/consult'
+
+defineProps<{ item: Doctor }>()
+
+// 关注逻辑
+const { loading, follow } = useFollow()
+</script>
+
+<template>
+  <div class="doctor-card">
+    <van-image round :src="item.avatar" />
+    <p class="name">{{ item.name }}</p>
+    <p class="van-ellipsis">{{ item.hospitalName }} {{ item.depName }}</p>
+    <p>{{ item.positionalTitles }}</p>
+    <van-button
+      :loading="loading"
+      round
+      size="small"
+      type="primary"
+      @click="follow(item)"
+    >
+      {{ item.likeFlag === 1 ? '已关注' : '+ 关注' }}
+    </van-button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.doctor-card {
+  width: 135px;
+  height: 190px;
+  background: #fff;
+  border-radius: 20px;
+  box-shadow: 0px 0px 11px 0px rgba(229, 229, 229, 0.2);
+  text-align: center;
+  padding: 15px;
+  margin-left: 15px;
+  display: inline-block;
+  box-sizing: border-box;
+  > .van-image {
+    width: 58px;
+    height: 58px;
+    vertical-align: 50%;
+    margin: 0 auto 8px;
+  }
+  > p {
+    margin-bottom: 0;
+    font-size: 11px;
+    color: var(--cp-tip);
+    &.name {
+      font-size: 13px;
+      color: var(--cp-text1);
+      margin-bottom: 5px;
+    }
+  }
+  > .van-button {
+    height: 28px;
+    width: 72px;
+    padding: 0 12px;
+    margin-top: 8px;
+  }
+}
+</style>
