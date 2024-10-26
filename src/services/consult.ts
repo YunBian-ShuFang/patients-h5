b38cac2d55e011f7313d78eb0dd767e9d1b17ddd
kohane <13542031914@163.com>
2024-10-26 16:28:55 +0800

    病情描述-图片上传

--- src/services/consult.ts
+++ src/services/consult.ts
@@ -2,3 +2,14 @@ import { instance } from '@/utils/request'
 
 // 获取所有科室
 export const getAllDep = () => instance({ url: '/dep/all', method: 'get' })
+
+// 上传图片
+export const uploadImage = (file: File) => {
+  const fd = new FormData()
+  fd.append('file', file)
+  return instance({
+    url: '/upload',
+    method: 'post',
+    data: fd
+  })
+}
