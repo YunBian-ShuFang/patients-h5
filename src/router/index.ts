17536c625da1139bd08160c1ace7f018d8f81f19
kohane <13542031914@163.com>
2025-03-13 02:45:33 +0800

    chore：修改vue router 为 Hash 模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { useUserStore } from '@/stores'
 import { createRouter, createWebHistory } from 'vue-router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import { createWebHashHistory } from 'vue-router'
 
 // createRouter 创建路由实例，===> new VueRouter()
 // history 是路由模式，hash模式，history模式
@@ -14,7 +15,7 @@ import 'nprogress/nprogress.css'
 // vite.config.ts  添加配置  base: my-path，路由这就会加上 my-path 前缀了
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     // 首页布局容器
     {
